d81408869b7ac9d01d4d6a2191cf8977f8482eec
Lautaro Dragan <lautaro.dragan@gmail.com>
2019-11-13 15:51:25 -0300

    .

--- PoetRegistry.sol
+++ PoetRegistry.sol
@@ -7,13 +7,16 @@ contract PoetRegistry {
 
   address public _owner;
   CID[] public cids;
+  
+  event CidAdded(string cid, uint index);
 
   constructor() public {
     _owner = msg.sender;
   }
 
   function addCid(string memory cid) public {
-    cids.push(CID(cid));
+    uint length = cids.push(CID(cid));
+    emit CidAdded(cid, length - 1);
   }
 
   function getCidCount() public view returns (uint) {
